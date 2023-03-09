d2b09d873cbd3299c157d3616d3d35be1a241e98
Bakirovildar <bakirovildar2011@mail.ru>
2023-03-09 13:58:11 +0500

    first commit

---
+++ src/components/Header/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <InfoNav/>
+  </div>
+</template>
+
+<script>
+import InfoNav from "@/components/Header/HeaderInfo/InfoNav";
+
+export default {
+  components: {InfoNav}
+}
+</script>
+
+<style scoped>
+
+</style>
