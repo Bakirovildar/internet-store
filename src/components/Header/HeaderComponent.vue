49891bb4722180515eb3d6a89450015f44a648cf
Bakirovildar <bakirovildar2011@mail.ru>
2023-03-10 09:08:23 +0500

    add header

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -2,15 +2,17 @@
   <div class="container-fluid">
     <InfoNav/>
     <HeaderMain/>
+    <HeaderCategory/>
   </div>
 </template>
 
 <script>
 import InfoNav from "@/components/Header/HeaderInfo/InfoNav";
 import HeaderMain from "@/components/Header/HeaderMain/HeaderMain";
+import HeaderCategory from "@/components/Header/HeaderCategory/HeaderCategory";
 
 export default {
-  components: {HeaderMain, InfoNav}
+  components: {HeaderCategory, HeaderMain, InfoNav}
 }
 </script>
 
---
+++ src/icons/IconDown.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" viewBox="0 0 24 24" fill="none">
+    <path d="M8 10L12 14L16 10" stroke="#FFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
