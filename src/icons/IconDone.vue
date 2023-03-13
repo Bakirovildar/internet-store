5020ec6f760c6afd861db7921586e8c6dd2ff32a
Bakirovildar <bakirovildar2011@mail.ru>
2023-03-13 13:24:43 +0500

    add icons for featured component

---
+++ src/icons/IconDone.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 24 24">
+    <path d="M9 16.2L4.8 12l-1.4 1.4L9 19L21 7l-1.4-1.4L9 16.2z" :fill="color"></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ['color']
+}
+</script>
+
+<style scoped>
+
+</style>
